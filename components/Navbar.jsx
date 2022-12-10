5c192a8810d41a38b9344e45d5be2e4f9b320f97
abhijit <abhijitnath1a1@gmail.com>
2022-12-10 10:00:59 +0530

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Kamuy Shop</Link>
+        <Link href="/">Kamuy Store</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
